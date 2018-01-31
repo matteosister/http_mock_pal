8a0ebec8cb5d615ccb6f83d14af7710a654ea6dd
Matteo Giachino <matteog@gmail.com>
2018-01-31 23:14:31 +0100

    pre publish setup

--- mix.exs
+++ mix.exs
@@ -4,13 +4,30 @@ defmodule HttpMockPal.Mixfile do
   def project do
     [
       app: :http_mock_pal,
+      description: description(),
+      package: package(),
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "postgrex",
+      # These are the default files included in the package
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Matteo Giachino"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/matteosister/http_mock_pal"}
+    ]
+  end
+
+  defp description(),
+    do: "A convenience library to spawn plug router with your application for testing purposes"
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(env) when env in ~w(dev test)a, do: ["lib", "test/support"]
@@ -33,6 +50,6 @@ defmodule HttpMockPal.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:cowboy, "~> 1.0"}, {:plug, "~> 1.0"}]
+    [{:cowboy, "~> 1.0"}, {:plug, "~> 1.0"}, {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
