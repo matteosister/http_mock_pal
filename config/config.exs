e720dda921c925d0c4e9f85da2609d3ce0c35e1d
Matteo Giachino <matteog@gmail.com>
2018-01-31 23:06:13 +0100

    removed example config

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,8 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :http_mock_pal,
-  routers: [{HttpMockPal.TestRouter, port: 4010}]
+# config :http_mock_pal,
+#  routers: [{HttpMockPal.TestRouter, port: 4010}]
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
